3a8e5b14a8b5ab6d3c55dab2c734b593bae07e21
Takuya Matsuyama <nora@odoruinu.net>
2017-09-05 11:51:55 +0900

    Add testimonials

--- components/top-page/testimonials.js
+++ components/top-page/testimonials.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './testimonials.scss'
-// import OutboundLink from '../outbound-link'
+import OutboundLink from '../outbound-link'
 
 export default class TestimonialsSection extends Component {
   static propTypes = {
@@ -11,22 +11,52 @@ export default class TestimonialsSection extends Component {
   render () {
     return (
       <section className='white-bg light testimonials-section'>
-        <h2>What Customers Say</h2>
-        <div className='ui text container'>
-          <i className='quote left icon' />
-          <div className='testimonial-item'>
-            <p>
-              Your application is a life changer.
-              I&apos;ve tried numerous markdown based applications over the years and I&apos;m so pleased to finally find a keeper!
-              Awesome work!
-            </p>
-            <div className='testmonial-by'>
-              — <strong>James Lilliott</strong>
-              <img src='/testimonials/james.jpg' />
+        <h2>Inkdrop is made for developers like you</h2>
+        <div className='ui container'>
+          <div className='ui grid three columns'>
+            <div className='column'>
+              <div className='testimonial-item'>
+                <p>
+                  Your application is a life changer.
+                  I&apos;ve tried numerous markdown based applications over the years and I&apos;m so pleased to finally find a keeper!
+                  Awesome work!
+                </p>
+                <div className='testmonial-by'>
+                  <img src='/testimonials/james.jpg' />
+                  — <strong>James Lilliott</strong>.
+                </div>
+              </div>
             </div>
-          </div>
-          <div className='right aligned'>
-            <i className='quote right icon' />
+            <div className='column'>
+              <div className='testimonial-item'>
+                <p>
+                  Inkdrop is all very user-friendly and looks absolutely fantastic.
+                </p>
+                <div className='testmonial-by'>
+                  <img src='/testimonials/john.png' />
+                  — <strong>
+                    <OutboundLink href='http://alternativeto.net/software/inkdrop/reviews/'>
+                      John Fastman
+                    </OutboundLink>
+                  </strong>.
+                </div>
+              </div>
+            </div>
+            <div className='column'>
+              <div className='testimonial-item'>
+                <p>
+                  The search for the perfect note-taking app is finally over.
+                  It&apos;s elegant, well designed, simple, and functional.
+                </p>
+                <div className='testmonial-by'>
+                  — <strong>
+                    <OutboundLink href='http://alternativeto.net/software/inkdrop/reviews/'>
+                      fletcher
+                    </OutboundLink>
+                  </strong>.
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
