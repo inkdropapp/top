292114617ac9de28f82e7a745a558993e9056731
Takuya Matsuyama <nora@odoruinu.net>
2017-12-05 11:35:03 +0900

    Stripe required platform versions

--- components/top-page/desktop-description.js
+++ components/top-page/desktop-description.js
@@ -32,17 +32,14 @@ export default class DesktopDescription extends Component {
           <SupportingPlatform
             image='/top/apple-logo-small.png'
             platform='macOS'
-            versions='(El Capitan)'
           />
           <SupportingPlatform
             image='/top/windows-logo-small.png'
             platform='Windows'
-            versions='7+ (64bit)'
           />
           <SupportingPlatform
             image='/top/linux-logo-small.png'
             platform='Linux'
-            versions='(Ubuntu, 64bit)'
           />
           <div className='link-demo'>
             <a href='https://app.inkdrop.info/demo'>
