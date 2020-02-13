f683de7bbf27546815602b31c4b59537b9f6a8c0
Takuya Matsuyama <hi@craftz.dog>
2020-02-13 16:52:12 +0900

    feat(get-started): add links to discounts

--- src/components/section-get-started.js
+++ src/components/section-get-started.js
@@ -4,6 +4,7 @@ import './section-get-started.less'
 import Img from 'gatsby-image'
 import SignupButton from './signup-button'
 import Container from 'semantic-ui-react/dist/es/elements/Container'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 export default class GetNowSection extends Component {
   render() {
@@ -31,6 +32,16 @@ export default class GetNowSection extends Component {
                         See pricing&nbsp;
                         <i className="angle right icon" />
                       </Link>
+                    </div>
+                    <div className="link-to-discount">
+                      Need discount? Check out:&nbsp;
+                      <OutboundLink href="https://docs.inkdrop.app/student-discount">
+                        Educational discount
+                      </OutboundLink>
+                      &nbsp;&amp;&nbsp;
+                      <OutboundLink href="https://blog.inkdrop.info/announcing-inkdrop-plugin-developer-license-d5c9be52f8af">
+                        plugin developer license
+                      </OutboundLink>
                     </div>
                   </div>
                 </div>
