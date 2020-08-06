ee95031c3b80d47099581b2f034144d4005848ce
Takuya Matsuyama <hi@craftz.dog>
2020-08-06 13:04:22 +0900

    feat(sync): add icons

--- src/components/top-section-seamless-sync.js
+++ src/components/top-section-seamless-sync.js
@@ -1,59 +1,41 @@
 import './top-section-seamless-sync.less'
-import { StaticQuery, graphql } from 'gatsby'
 import React from 'react'
 import Container from 'semantic-ui-react/dist/es/elements/Container'
-import Img from 'gatsby-image'
+import { ReactComponent as SyncImage } from '../images/icons/sync.svg'
+import { ReactComponent as SecureImage } from '../images/icons/secure.svg'
+import { ReactComponent as WorkImage } from '../images/icons/work.svg'
 
 class SectionSeamlessSync extends React.Component {
   componentDidMount() {}
   render() {
     return (
-      <StaticQuery
-        query={query}
-        render={data => (
-          <section id="seamless-sync">
-            <Container text>
-              <h2 className="ui icon header">
-                <i className="cloud icon" />
-                <div className="content">Have it everywhere, securely</div>
-              </h2>
-              <p>
-                Capture a note once, and it’s instantly available on all your
-                devices. It syncs data securely with end-to-end encryption.
-              </p>
-              <p>
-                <a
-                  className="ui rounded white button"
-                  href="https://docs.inkdrop.app/security"
-                >
-                  Learn more about security&nbsp;
-                  <i className="angle double right icon" />
-                </a>
-              </p>
-            </Container>
-            <Container>
-              <Img
-                fluid={data.image01.childImageSharp.fluid}
-                alt="App running on multiple computers"
-              />
-            </Container>
-          </section>
-        )}
-      />
+      <section id="seamless-sync">
+        <Container text>
+          <div className="image-container">
+            <SyncImage className="small svg-icon" />
+            <WorkImage className="svg-icon" />
+            <SecureImage className="small svg-icon" />
+          </div>
+          <h2 className="ui icon header">
+            <div className="content">Have it everywhere, securely</div>
+          </h2>
+          <p>
+            Capture a note once, and it’s instantly available on all your
+            devices. It syncs data securely with end-to-end encryption.
+          </p>
+          <p>
+            <a
+              className="ui rounded white button"
+              href="https://docs.inkdrop.app/security"
+            >
+              Learn more about security&nbsp;
+              <i className="angle double right icon" />
+            </a>
+          </p>
+        </Container>
+      </section>
     )
   }
 }
 
 export default SectionSeamlessSync
-
-const query = graphql`
-  query {
-    image01: file(relativePath: { eq: "have-it-everywhere.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 2000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
