5922f0f59094090dbbfba51c449c5b9cd72567dc
Takuya Matsuyama <hi@craftz.dog>
2020-08-14 12:47:30 +0900

    build(image): change image quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,12 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaultQuality: 70
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
