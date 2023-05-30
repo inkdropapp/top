9606723ee0a7e378e59834437180cc8df7bf1207
Takuya Matsuyama <hi@craftz.dog>
2023-05-30 22:50:47 +0900

    feat(seamless-sync): add a link to the note-taking tip page

--- src/components/top-section-seamless-sync.js
+++ src/components/top-section-seamless-sync.js
@@ -2,30 +2,67 @@ import './top-section-seamless-sync.less'
 import React from 'react'
 import Container from 'semantic-ui-react/dist/es/elements/Container'
 import SectionFourth from './section-fourth'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import OutboundLink from './outbound-link'
 import { ReactComponent as SyncImage } from '../images/icons/sync-color.svg'
 import { ReactComponent as SecureImage } from '../images/icons/secure-color.svg'
 import { ReactComponent as SelfHostedImage } from '../images/icons/self-hosted-color.svg'
 
 const SectionSeamlessSync = () => {
+  const data = useStaticQuery(query)
   return (
     <SectionFourth id="seamless-sync">
+      <Container>
+        <h2 className="ui icon header">
+          <div className="content">Boost your engineering process</div>
+        </h2>
+        <div className="masthead-container">
+          <div className="ui stackable grid">
+            <div className="eight wide column">
+              <div className="masthead-video">
+                <iframe
+                  className="video"
+                  src="https://www.youtube.com/embed/-qBavwqc_mY"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <div className="caption">
+                <OutboundLink
+                  className="white link"
+                  href="https://www.youtube.com/watch?v=-qBavwqc_mY"
+                >
+                  How to take notes in Markdown (Video)&nbsp;
+                  <i className="angle double right icon" />
+                </OutboundLink>
+              </div>
+            </div>
+            <div className="eight wide column">
+              <Link to="/note-taking-tips/">
+                <GatsbyImage
+                  alt={'Tips'}
+                  className={`cover-image`}
+                  image={data.tipsCover.childImageSharp.gatsbyImageData}
+                />
+              </Link>
+              <div className="caption">
+                <Link to="/note-taking-tips/" className="white link">
+                  Effective note-taking in coding and learning&nbsp;
+                  <i className="angle double right icon" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
       <Container>
         <h2 className="ui icon header">
           <div className="content">
             At work, at home, and everywhere in between
           </div>
         </h2>
-        <div className="masthead-container">
-          <div className="masthead-video">
-            <iframe
-              className="video"
-              src="https://www.youtube.com/embed/-qBavwqc_mY"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
       </Container>
       <Container className="description">
         <div className="ui stackable three column grid">
@@ -98,3 +135,13 @@ const SectionSeamlessSync = () => {
 }
 
 export default SectionSeamlessSync
+
+const query = graphql`
+  query {
+    tipsCover: file(relativePath: { eq: "tips/cover.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: CONSTRAINED, width: 1800)
+      }
+    }
+  }
+`
