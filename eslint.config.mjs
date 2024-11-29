2ab4d02a9a883c8f144db36641adbfc07e855d99
Takuya Matsuyama <hi@craftz.dog>
2024-11-29 10:23:06 +0900

    chore(eslint): bump up

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import react from 'eslint-plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [
+  {
+    ignores: ['public', '.cache']
+  },
+  ...compat.extends('plugin:react/recommended', 'prettier'),
+  {
+    plugins: {
+      react
+    },
+
+    languageOptions: {
+      globals: {}
+    },
+
+    rules: {
+      'no-useless-escape': 0,
+      'prefer-const': 2,
+
+      'no-unused-vars': [
+        2,
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+
+      'react/prop-types': 0
+    },
+
+    settings: {
+      react: {
+        version: '18'
+      }
+    }
+  }
+]
